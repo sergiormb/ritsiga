dea525bad94561eed7352d227f1dab94d7e0b602
Sergio Pino Marquez <i02pimas@uco.es>
2015-04-12 20:52:32 +0200

    Corregido error en la estructura del proyecto

---
+++ src/AppBundle/Doctrine/ORM/UserRepository.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tfg
+ * Date: 8/04/15
+ * Time: 20:00
+ */
+
+namespace AppBundle\Doctrine\ORM;
+
+
+use Doctrine\ORM\EntityRepository;
+
+class UserRepository extends EntityRepository
+{
+
+}
