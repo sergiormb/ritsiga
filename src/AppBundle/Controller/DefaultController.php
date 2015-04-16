b06b2b7c79fe030a3e6133ca32edea76c46bc31f
Sergio Pino Marquez <i02pimas@uco.es>
2015-04-16 23:13:54 +0200

    Primeras pruebas para visualización de la página principal. Ver #5

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,6 +14,6 @@ class DefaultController extends Controller
     public function indexAction()
     {
         $conventions = $this->getDoctrine()->getRepository('AppBundle:Convention')->findConventionsAvailables();
-	    return $this->render('Default/index.html.twig', array('conventions' => $conventions));
+	    return $this->render('Conventions/list_convetions.html.twig', array('conventions' => $conventions));
     }
 }
