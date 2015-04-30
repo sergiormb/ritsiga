d69af0630e5c1c2fef5836e5135ca11d162f28fa
Sergio Pino Marquez <i02pimas@uco.es>
2015-04-30 22:01:48 +0200

    Bundle del menú Ver #5

---
+++ src/AppBundle/Menu/Builder.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tfg
+ * Date: 29/04/15
+ * Time: 23:53
+ */
+
+namespace AppBundle\Menu;
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class Builder extends ContainerAware
+{
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->setChildrenAttributes(array('class' => 'sidebar-menu'));
+
+        $menu->addChild('Inicio', array('route' => 'homepage'));
+
+        return $menu;
+    }
+}
