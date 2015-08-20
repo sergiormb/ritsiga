99218e7855fa2d7f56ea679d3aaec7ff9ecda392
Sergio Pino Marquez <i02pimas@uco.es>
2015-08-20 19:22:27 +0200

    Quitada contraseña en el form de edit user

--- src/AppBundle/Form/Type/ProfileType.php
+++ src/AppBundle/Form/Type/ProfileType.php
@@ -13,18 +13,21 @@ use Symfony\Component\Form\FormBuilderInterface;
 
 class ProfileType extends BaseType
 {
-    public function buildUserForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        parent::buildUserForm($builder, $options);
 
         $builder->add('firstname', null, array('label' => 'label.name'));
         $builder->add('lastname', null, array('label' => 'label.last_name'));
-        $builder->add('university','entity', array('class' => 'AppBundle\Entity\University', 'label' => 'label.university'));
-        $builder->add('college','entity', array('class' => 'AppBundle\Entity\College', 'label' => 'label.college'));
-        $builder->add('studentdelegation','entity', array('class' => 'AppBundle\Entity\StudentDelegation', 'label' => 'label.student_delegation'));
+        $builder->add('university','entity', array('class' => 'AppBundle\Entity\University',
+            'label' => 'label.university'));
+        $builder->add('college','entity', array('class' => 'AppBundle\Entity\College',
+            'label' => 'label.college'));
+        $builder->add('studentdelegation','entity', array('class' => 'AppBundle\Entity\StudentDelegation',
+            'label' => 'label.student_delegation'));
         $builder->add('email');
         $builder->add('phone', null, array('label' => 'label.phone'));
-        $builder->add('gender', 'choice', array('label' => 'label.gender', 'choices'  => array('H' => 'Hombre', 'M' => 'Mujer'),
+        $builder->add('gender', 'choice', array('label' => 'label.gender',
+            'choices'  => array('H' => 'Hombre', 'M' => 'Mujer'),
             'required' => true));
         $builder->add('website', null, array('label' => 'label.website'));
     }
