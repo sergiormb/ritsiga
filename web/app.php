85c36e55057b3d339f7c69bcfa91b565cb45b6ab
Sergio Pino Marquez <i02pimas@uco.es>
2015-08-28 20:13:35 +0200

    Añadido debug

--- web/app.php
+++ web/app.php
@@ -1,24 +1,31 @@
 <?php
+//libxml_disable_entity_loader(false);
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
+use Symfony\Component\Debug\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
+
+$env = getenv('SYMFONY_ENV') ?: 'prod';
+if ($debug = 'dev' === $env) {
+    Debug::enable();
+}
 
 // Enable APC for autoloading to improve performance.
 // You should change the ApcClassLoader first argument to a unique prefix
 // in order to prevent cache key conflicts with other applications
 // also using APC.
-/*
-$apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
+if ('prod' === $env) {
+    $apcLoader = new ApcClassLoader(sha1(__FILE__), $loader);
+    $loader->unregister();
+    $apcLoader->register(true);
+}
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel($env, $debug);
 $kernel->loadClassCache();
 //$kernel = new AppCache($kernel);
 
